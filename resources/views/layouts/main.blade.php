3132cc6d30b78305f40e61f22281fa31c98d0b51
Rostyslav <bortnikrostislav370@gmail.com>
2024-08-06 01:27:53 +0300

    Bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,18 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
-<div>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{ route('main.index') }}">Main</a></li>
-                <li><a href="{{ route('post.index') }}">Posts</a></li>
-                <li><a href="{{ route('about.index') }}">About</a></li>
-                <li><a href="{{ route('contact.index') }}">Contacts</a></li>
-            </ul>
+<div class="container">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <li><a class="nav-link active"href="{{ route('main.index') }}">Main</a></li>
+                    <li><a class="nav-link" href="{{ route('post.index') }}">Posts</a></li>
+                    <li><a class="nav-link" href="{{ route('about.index') }}">About</a></li>
+                    <li><a class="nav-link" href="{{ route('contact.index') }}">Contacts</a></li>
+                </div>
+            </div>
         </nav>
     </div>
     @yield('content')
