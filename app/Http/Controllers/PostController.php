ecf1f919fa3fee5faa8f0ba3b9a21d4220617e83
Rostyslav <bortnikrostislav370@gmail.com>
2024-08-10 00:28:53 +0300

    CRUD - Updating the interface 3.0

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -54,8 +54,9 @@ class PostController extends Controller
     public function edit(Post $post)
     {
         $categories = Category::all();
+        $tags = Tag::all();
 
-        return view('post.edit', compact('post', 'categories'));
+        return view('post.edit', compact('post', 'categories', 'tags'));
     }
 
     public function update(Post $post)
@@ -66,10 +67,13 @@ class PostController extends Controller
             'content' => 'string',
             'image' => 'string',
             'category_id' => '',
+            'tags' => '',
         ]);
+        $tags = $data['tags'];
+        unset($data['tags']);
 
         $post->update($data);
-
+        $post->tags()->sync($tags);
         return redirect()->route('post.show', $post->id);
 
     }
